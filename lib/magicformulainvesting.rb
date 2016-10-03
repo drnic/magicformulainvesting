7a196f8079e8658a902af47dc01d25c26a59b633
Dr Nic Williams <drnicwilliams@gmail.com>
2016-10-03 21:36:22 +1000

    display results as a table

--- lib/magicformulainvesting.rb
+++ lib/magicformulainvesting.rb
@@ -1,10 +1,14 @@
 require "magicformulainvesting/version"
+require "magicformulainvesting/company_quote"
 
 require "mechanize"
+require "terminal-table"
 
 module MagicFormulaInvesting
   extend self
   def run
+    company_quotes = []
+
     agent = Mechanize.new
     agent.get('https://www.magicformulainvesting.com/Account/LogOn') do |login_page|
       page = login_page.form_with(action: '/Account/Logon') do |f|
@@ -21,8 +25,22 @@ module MagicFormulaInvesting
       results_table = results_page.search("table")[-1]
       results_rows = results_table.search("tbody tr")
 
-      pp results_rows
-      puts results_rows.to_html
+      results_rows.each do |row|
+        data = row.search("td")
+        quote = MagicFormulaInvesting::CompanyQuote.new
+        quote.name = data[0].text
+        quote.ticket = data[1].text
+        quote.market_cap = data[2].text
+        quote.price_from = data[3].text
+        quote.most_recent_quarter_data = data[4].text
+        company_quotes << quote
+      end
     end
+
+    table = Terminal::Table.new :headings => ['Name', 'Ticket', 'Market Cap', 'Price From', 'Most Recent Quarter Data']
+    company_quotes.each do |quote|
+      table << [quote.name, quote.ticket, quote.market_cap, quote.price_from, quote.most_recent_quarter_data]
+    end
+    puts table
   end
 end
---
+++ lib/magicformulainvesting/company_quote.rb
@@ -0,0 +1,9 @@
+module MagicFormulaInvesting
+  class CompanyQuote
+    attr_accessor :name
+    attr_accessor :ticket
+    attr_accessor :market_cap
+    attr_accessor :price_from
+    attr_accessor :most_recent_quarter_data
+  end
+end
